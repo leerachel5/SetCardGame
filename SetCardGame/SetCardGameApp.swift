36fe17576a31da84303e32f352152e5dbb4a8990
rlee17 <rachel_lee@intuit.com>
2024-07-13 16:47:03 -0700

    initial commit

---
+++ SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by Rachel Lee on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SetCardGameView()
+        }
+    }
+}
---
+++ SetCardGame/SetCardGameView.swift
@@ -0,0 +1,18 @@
+//
+//  SetCardGameView.swift
+//  SetCardGame
+//
+//  Created by Rachel Lee on 7/13/24.
+//
+
+import SwiftUI
+
+struct SetCardGameView: View {
+    var body: some View {
+        Text("Set")
+    }
+}
+
+#Preview {
+    SetCardGameView()
+}
