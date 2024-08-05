b10ce6061c1fd83cac21ca061d0d2828d380d9b5
leerachel5 <leerachel481@gmail.com>
2024-08-05 01:19:20 -0700

    prevent selection of matched cards

--- SetCardGame/Model/SetCardGame.swift
+++ SetCardGame/Model/SetCardGame.swift
@@ -61,7 +61,27 @@ struct SetCardGame<Number: SetCardGameFeature.Number, Shape: SetCardGameFeature.
         return cards[partition]!
     }
     
-    // MARK: Mutating Methods
+    private func getSelectedCards() -> Array<Card> {
+        return cards[.faceUp]!.filter({ $0.selected })
+    }
+    
+    private func match<CardCollection: Collection<Card>>(cards: CardCollection) -> Bool {
+        guard cards.count == 3 else { return false }
+        
+        let numbers = cards.map({ $0.number })
+        let shapes = cards.map({ $0.shape })
+        let shadings = cards.map({ $0.shading })
+        let colors = cards.map({ $0.color })
+        
+        let matched = (numbers.allTheSame() || numbers.allDifferent()) &&
+                      (shapes.allTheSame() || shapes.allDifferent()) &&
+                      (shadings.allTheSame() || shadings.allDifferent()) &&
+                      (colors.allTheSame() || colors.allDifferent())
+        
+        return matched
+    }
+    
+    // MARK: Mutator Methods
     mutating func draw(count: Int) {
         for _ in 0..<count {
             moveRandomCard(from: .deck, to: .faceUp)
@@ -71,30 +91,31 @@ struct SetCardGame<Number: SetCardGameFeature.Number, Shape: SetCardGameFeature.
     mutating func select(_ card: Card) {
         guard card.partition == .faceUp else { return }
         
-        var selectedCards = cards[card.partition]!.filter({ $0.selected })
+        var selectedCards = getSelectedCards()
         if selectedCards.count == 3 {
-            for card in selectedCards {
-                if card.matched == true {
-                    moveCard(card, to: .discarded)
-                } else if card.matched == false {
-                    if let cardIndex = cards[card.partition]!.firstIndex(where: { $0.id == card.id }) {
-                        cards[card.partition]![cardIndex].matched = nil
-                        cards[card.partition]![cardIndex].selected = false
+            for selectedCard in selectedCards {
+                if selectedCard.matched == true {
+                    if selectedCards.contains(card) { return }
+                    moveCard(selectedCard, to: .discarded)
+                } else if selectedCard.matched == false {
+                    if let cardIndex = cards[.faceUp]!.firstIndex(where: { $0.id == selectedCard.id }) {
+                        cards[.faceUp]![cardIndex].matched = nil
+                        cards[.faceUp]![cardIndex].selected = false
                     }
                 }
             }
         }
         
-        guard let chosenIndex = cards[card.partition]!.firstIndex(where: { $0.id == card.id }) else { return }
-        cards[card.partition]![chosenIndex].selected.toggle()
+        guard let chosenIndex = cards[.faceUp]!.firstIndex(where: { $0.id == card.id }) else { return }
+        cards[.faceUp]![chosenIndex].selected.toggle()
         
-        selectedCards = cards[card.partition]!.filter({ $0.selected })
+        selectedCards = getSelectedCards()
         
         if selectedCards.count == 3 {
             let matched = match(cards: selectedCards)
             for card in selectedCards {
-                if let selectedCardIndex = cards[card.partition]!.firstIndex(where: { $0.id == card.id }) {
-                    cards[card.partition]![selectedCardIndex].matched = matched
+                if let selectedCardIndex = cards[.faceUp]!.firstIndex(where: { $0.id == card.id }) {
+                    cards[.faceUp]![selectedCardIndex].matched = matched
                 }
             }
         }
@@ -113,21 +134,5 @@ struct SetCardGame<Number: SetCardGameFeature.Number, Shape: SetCardGameFeature.
     private mutating func moveRandomCard(from origin: Card.Partition, to destination: Card.Partition) {
         guard let cardToMove = cards[origin]?.randomElement() else { return }
         moveCard(cardToMove, to: destination)
-    }
-    
-    mutating private func match<CardCollection: Collection<Card>>(cards: CardCollection) -> Bool {
-        guard cards.count == 3 else { return false }
-        
-        let numbers = cards.map({ $0.number })
-        let shapes = cards.map({ $0.shape })
-        let shadings = cards.map({ $0.shading })
-        let colors = cards.map({ $0.color })
-        
-        let matched = (numbers.allTheSame() || numbers.allDifferent()) &&
-                      (shapes.allTheSame() || shapes.allDifferent()) &&
-                      (shadings.allTheSame() || shadings.allDifferent()) &&
-                      (colors.allTheSame() || colors.allDifferent())
-        
-        return matched
     }
 }
